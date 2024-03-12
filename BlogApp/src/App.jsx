6b2b6087c03f62292ea134a5945fd97f0524be6a
zubair-shabir <zubairshabir980@gmail.com>
2024-03-12 15:21:47 +0530

    Project Setup and appwrite configration

---
+++ BlogApp/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import conf from "./conf/config";
+
+function App() {
+  console.log(conf.appwriteUrl);
+  return (
+    <>
+      <h1> Blog with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
