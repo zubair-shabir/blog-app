8d623ebe1205362bce713acd711cd774fa13166a
zubair-shabir <zubairshabir980@gmail.com>
2024-03-21 16:31:48 +0530

    singin and signup form created

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
-import Logo from "./logo";
+import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer, Container, Logo,LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn };
