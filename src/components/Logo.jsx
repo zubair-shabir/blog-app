d76211be94f3336fbab83bb35e4af030e22ac600
zubair-shabir <zubairshabir980@gmail.com>
2024-05-02 21:30:26 +0530

    Logo and title added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Logo = ({ width = "100px" }) => {
-  return <div>logo</div>;
+  return (
+    <div>
+      <img src="./logo.png" className="w-24" alt="Logo" />
+    </div>
+  );
 };
 
 export default Logo;
